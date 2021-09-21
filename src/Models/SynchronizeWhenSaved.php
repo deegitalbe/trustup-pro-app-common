59cc534fd7f881dc926496ac001c54b0d4b3a5d4
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-09-21 17:09:32 +0200

    ✨ Adding trait used to watch account saved event and call synchronizer.

---
+++ src/Models/SynchronizeWhenSaved.php
@@ -0,0 +1,17 @@
+<?php
+namespace Henrotaym\AccountSynchronizer\Models;
+
+use Henrotaym\AccountSynchronizer\Contracts\AccountContract;
+
+/**
+ * Synchronizing account when saved event occurs.
+ */
+trait SynchronizeWhenSaved
+{
+    protected static function bootSynchronizeWhenSaved()
+    {
+        static::saved(function (AccountContract $account) {
+            app()->make(SynchronizerContract::class)->update($account);
+        });
+    }
+}
