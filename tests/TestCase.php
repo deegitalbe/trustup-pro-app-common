0cca38d9aa981753203253c1bcc8311198bcfe3c
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-10-27 16:18:27 +0200

    🐛 Missing services providers in testcase.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,16 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 use Henrotaym\LaravelHelpers\Providers\HelperServiceProvider;
 use Henrotaym\LaravelApiClient\Providers\ClientServiceProvider;
 use Deegitalbe\TrustupProAppCommon\Providers\AppAccountServiceProvider;
+use Deegitalbe\ServerAuthorization\Providers\ServerAuthorizationServiceProvider;
+use Deegitalbe\TrustupVersionedPackage\Providers\TrustupVersionedPackageServiceProvider;
 
 class TestCase extends BaseTestCase
 {
     protected function getPackageProviders($app)
     {
         return [
+            TrustupVersionedPackageServiceProvider::class,
+            ServerAuthorizationServiceProvider::class,
             ClientServiceProvider::class,
             AppAccountServiceProvider::class,
             HelperServiceProvider::class
