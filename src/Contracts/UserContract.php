239460e0ee6b1620f16132593f01fe87662aebe5
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-11-12 15:26:07 +0100

    👔 Creating contracts for trustup.pro api.

---
+++ src/Contracts/UserContract.php
@@ -0,0 +1,62 @@
+<?php
+namespace Deegitalbe\TrustupProAppCommon\Contracts;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Deegitalbe\TrustupProAppCommon\Contracts\AccountContract;
+use Deegitalbe\TrustupProAppCommon\Contracts\ProfessionalContract;
+
+/**
+ * Representing a user.
+ */
+interface UserContract extends Arrayable
+{
+    /**
+     * Getting id.
+     * 
+     * @return int
+     */
+    public function getId(): int;
+    
+    /**
+     * Getting name.
+     * 
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * Getting avatar.
+     * 
+     * @return string|null
+     */
+    public function getAvatar(): ?string;
+
+    /**
+     * Getting role associated with linked professional.
+     * 
+     * @return string|null
+     */
+    public function getRole(): ?string;
+
+    /**
+     * Getting linked professional.
+     * 
+     * @return ProfessionalContract
+     */
+    public function getProfessional(): ProfessionalContract;
+
+    /**
+     * Telling if user can access given account.
+     * 
+     * @return ProfessionalContract
+     */
+    public function hasAccessToAccount(AccountContract $account): bool;
+
+    /**
+     * Getting user from an attributes array.
+     * 
+     * @param array $attributes
+     * @return UserContract
+     */
+    public function fromArray(array $attributes): UserContract;
+}
