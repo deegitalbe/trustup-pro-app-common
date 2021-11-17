51afb008345d437658789103fb296dde2ff94fff
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-11-17 11:12:15 +0100

    ♻ SettingAccountAsEnvironment using authentication_related class.

--- src/Http/Middleware/SettingAccountAsEnvironment.php
+++ src/Http/Middleware/SettingAccountAsEnvironment.php
@@ -6,9 +6,25 @@ use Closure;
 use Hyn\Tenancy\Environment;
 use App\Http\AccountRequestManager;
 use Deegitalbe\TrustupProAppCommon\Facades\Package;
+use Deegitalbe\TrustupProAppCommon\Contracts\AuthenticationRelatedContract;
 
+/**
+ * Middleware setting up requested account as current environment.
+ */
 class SettingAccountAsEnvironment
 {
+    /**
+     * Authentication related actions.
+     * 
+     * @var AuthenticationRelatedContract
+     */
+    protected $authentication_related;
+    
+    public function __construct(AuthenticationRelatedContract $authentication_related)
+    {
+        $this->authentication_related = $authentication_related;
+    }
+    
     /**
      * Handle an incoming request.
      *
@@ -18,8 +34,12 @@ class SettingAccountAsEnvironment
      */
     public function handle($request, Closure $next)
     {
-        Package::environment()->tenant($request->requested_account);
-        Package::environment()->hostname($request->requested_account->hostnames()->first());
+        if (!$account = $this->authentication_related->getAccount()):
+            return response(['message' => "Could not understand which account should be used for this request."], 400);
+        endif;
+
+        Package::environment()->tenant($account);
+        Package::environment()->hostname($account->hostnames()->first());
 
         return $next($request);
     }
