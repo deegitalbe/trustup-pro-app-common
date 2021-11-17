ad4b768d9ef200b5324c721bd094b4772ffb3882
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-11-17 11:07:40 +0100

    🔥 Trustup pro api should not handle authentication related actions but only api related actions.

--- src/Contracts/Api/TrustupProApiContract.php
+++ src/Contracts/Api/TrustupProApiContract.php
@@ -4,29 +4,15 @@ namespace Deegitalbe\TrustupProAppCommon\Contracts\Api;
 use Deegitalbe\TrustupProAppCommon\Contracts\UserContract;
 use Deegitalbe\TrustupProAppCommon\Contracts\AccountContract;
 
+/**
+ * Representing actions that are available with trustup.pro
+ */
 interface TrustupProApiContract
 {
-    /**
-     * Making sure current request can access given account.
-     * 
-     * @param AccountContract $account
-     * @return bool Access success state.
-     */
-    public function hasAccessToAccount(AccountContract $account): bool;
-
     /**
      * Getting authenticated user linked current request.
      * 
      * @return UserContract|null
      */
     public function getUser(): ?UserContract;
-
-    /**
-     * Getting account linked to current request.
-     * 
-     * @param string|null $uuid If null, account header will be used.
-     * @param UserContract|null $user If null, user will be retrieved from trustup pro.
-     * @return Account|null Null if any error occured.
-     */
-    public function getAccount(?string $uuid = null, ?UserContract $user = null): ?AccountContract;
 }
