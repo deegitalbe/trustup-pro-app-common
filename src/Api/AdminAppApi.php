24a4e9f6d8815323edb7b775e8567049fc481be7
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-11-24 15:31:14 +0100

    👔 AdminAppApi using app model for retrieved apps + getting current app.

--- src/Api/AdminAppApi.php
+++ src/Api/AdminAppApi.php
@@ -2,10 +2,14 @@
 namespace Deegitalbe\TrustupProAppCommon\Api;
 
 use Illuminate\Support\Collection;
+use Deegitalbe\TrustupProAppCommon\Facades\Package;
+use Deegitalbe\TrustupProAppCommon\Contracts\AppContract;
 use Henrotaym\LaravelApiClient\Contracts\RequestContract;
-use Deegitalbe\TrustupProAppCommon\Contracts\Api\Client\AdminClientContract;
+use Henrotaym\LaravelApiClient\Contracts\ResponseContract;
 use Deegitalbe\TrustupProAppCommon\Contracts\Api\AdminAppApiContract;
 use Deegitalbe\TrustupProAppCommon\Exceptions\AdminAppApi\GetAppsException;
+use Deegitalbe\TrustupProAppCommon\Contracts\Api\Client\AdminClientContract;
+use Deegitalbe\TrustupProAppCommon\Exceptions\AdminAppApi\GetCurrentAppException;
 
 /**
  * Representing what is possible to do with admin API.
@@ -53,11 +57,11 @@ class AdminAppApi implements AdminAppApiContract
             return null;
         endif;
 
-        return collect($response->get()->data);
+        return $this->toApps($response);
     }
 
     /**
-     * Getting apps available in administration.
+     * Getting apps available in administration that are not dashboard related.
      * 
      * @return Collection|null null if any error.
      */
@@ -82,6 +86,41 @@ class AdminAppApi implements AdminAppApiContract
             return null;
         endif;
 
-        return collect($response->get()->data);
+        return $this->toApps($response);
+    }
+
+    /**
+     * Getting current app.
+     * 
+     * @return AppContract|null null if not found.
+     */
+    public function getCurrentApp(): ?AppContract
+    {
+        $request = app()->make(RequestContract::class)
+            ->setVerb('GET')
+            ->setUrl("api/apps")
+            ->addQuery(['is' => Package::appKey()]);
+
+        $response = $this->client->try($request, new GetCurrentAppException);
+
+        if ($response->failed()):
+            report($response->error());
+            return null;
+        endif;
+
+        return $this->toApps($response->response())->first();
+    }
+
+    /**
+     * Transforming raw apps to app models
+     * 
+     * @param array[array] $raw_apps
+     * @return Collection[AppContract]
+     */
+    protected function toApps(ResponseContract $response): Collection
+    {
+        return collect($response->get(true)['data'])->map(function(array $raw_app) {
+            return app()->make(AppContract::class)->setAttributes($raw_app);
+        });
     }
 }
--- src/Contracts/Api/AdminAppApiContract.php
+++ src/Contracts/Api/AdminAppApiContract.php
@@ -1,7 +1,9 @@
 <?php
 namespace Deegitalbe\TrustupProAppCommon\Contracts\Api;
 
+use stdClass;
 use Illuminate\Support\Collection;
+use Deegitalbe\TrustupProAppCommon\Contracts\AppContract;
 
 /**
  * Representing what is possible to do with admin app API.
@@ -21,4 +23,11 @@ interface AdminAppApiContract
      * @return Collection|null null if any error.
      */
     public function getAppsExceptDashboard(): ?Collection;
+
+    /**
+     * Getting current app.
+     * 
+     * @return AppContract|null null if any error.
+     */
+    public function getCurrentApp(): ?AppContract;
 }
