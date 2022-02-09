425a93aaf34ce60a8b3c607642d4409f2f44ec3b
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-02-09 12:12:00 +0100

    👔 Adding an update method updating account.

--- src/Projectors/Traits/AccountRelatedProjector.php
+++ src/Projectors/Traits/AccountRelatedProjector.php
@@ -4,6 +4,7 @@ namespace Deegitalbe\TrustupProAppCommon\Projectors\Traits;
 use Deegitalbe\TrustupProAppCommon\Contracts\AccountContract;
 use Deegitalbe\TrustupProAppCommon\Contracts\Query\AccountQueryContract;
 use Deegitalbe\TrustupProAppCommon\Contracts\Events\AccountRelatedEventContract;
+use Deegitalbe\TrustupProAppCommon\Contracts\Events\AccountUpdateEventContract;
 
 trait AccountRelatedProjector
 {
@@ -13,10 +14,25 @@ trait AccountRelatedProjector
      * @param AccountRelatedEventContract $event
      * @return AccountContract
      */
-    public function getAccount(AccountRelatedEventContract $event): AccountContract
+    public function getAccount(AccountRelatedEventContract $event): ?AccountContract
     {
         return app()->make(AccountQueryContract::class)
             ->whereUuid($event->getAccountUuid())
             ->first();
+    }
+
+    /**
+     * Updating account based on given event attributes.
+     * 
+     * @param AccountUpdateEventContract $event Related event.
+     * @return AccountContract|null Null if error.
+     */
+    public function updateAccount(AccountUpdateEventContract $event): ?AccountContract
+    {
+        if (!$account = $this->getAccount($event)):
+            return null;
+        endif;
+
+        return tap($account->fill($event->getAttributes()))->save();
     }
 }
