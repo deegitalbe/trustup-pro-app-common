7460450e14d0cdc492e28f77dffed325f6763b00
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-02-09 12:05:41 +0100

    🏷️ Adding an AccountUpdateRelatedEventContract to represent events updating account.

---
+++ src/Contracts/Events/AccountUpdateEventContract.php
@@ -0,0 +1,7 @@
+<?php
+namespace Deegitalbe\TrustupProAppCommon\Contracts\Events;
+
+/**
+ * Representing an event related to an existing account update.
+ */
+interface AccountUpdateEventContract extends AccountRelatedEventContract, HavingAttributesContract {}
