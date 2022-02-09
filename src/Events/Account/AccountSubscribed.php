49e1d038d517bfb3bb059d15f7a998bf39281164
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-02-09 12:09:15 +0100

    🏷️ Using new accountUpdateRelatedContract for AccountSubscribed.

--- src/Events/Account/AccountSubscribed.php
+++ src/Events/Account/AccountSubscribed.php
@@ -2,18 +2,15 @@
 
 namespace Deegitalbe\TrustupProAppCommon\Events\Account;
 
-use Deegitalbe\TrustupProAppCommon\Facades\Package;
 use Deegitalbe\TrustupProAppCommon\Events\ProjectorEvent;
-use Deegitalbe\ChargebeeClient\Chargebee\Models\Subscription;
 use Deegitalbe\TrustupProAppCommon\Events\Traits\HavingAttributes;
 use Deegitalbe\TrustupProAppCommon\Events\Traits\AccountRelatedEvent;
-use Deegitalbe\TrustupProAppCommon\Contracts\Events\HavingAttributesContract;
-use Deegitalbe\TrustupProAppCommon\Contracts\Events\AccountRelatedEventContract;
+use Deegitalbe\TrustupProAppCommon\Contracts\Events\AccountUpdateEventContract;
 
 /**
  * Event when account is getting a new subscription.
  */
-class AccountSubscribed extends ProjectorEvent implements AccountRelatedEventContract, HavingAttributesContract
+class AccountSubscribed extends ProjectorEvent implements AccountUpdateEventContract
 {
     use AccountRelatedEvent, HavingAttributes;
 }
