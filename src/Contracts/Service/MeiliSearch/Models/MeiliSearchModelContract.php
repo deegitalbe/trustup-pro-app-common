652d26297fa51daca0c76327d1f171339a1e6eb2
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-02-15 16:28:35 +0100

    👔 Meilisearch model contract.
    git status

---
+++ src/Contracts/Service/MeiliSearch/Models/MeiliSearchModelContract.php
@@ -0,0 +1,35 @@
+<?php
+namespace Deegitalbe\TrustupProAppCommon\Contracts\Service\MeiliSearch\Models;
+
+/**
+ * Representing a model that can be stored on Meilisearch DB.
+ */
+interface MeiliSearchModelContract
+{
+    /**
+     * Defining meiliSearch searchable attributes.
+     * 
+     * Order is important since search would be performed respecting given order.
+     *
+     * @return array
+     */
+    public function getMeiliSearchSearchableAttributes(): array;
+
+    /**
+     * Getting meiliSearch model unique identifier.
+     * 
+     * Identifier can be considered as MYSQL row ID.
+     *
+     * @return mixed
+     */
+    public function getMeiliSearchIndexPrimaryKey(): string;
+
+    /**
+     * Getting MeiliSearch model index.
+     * 
+     * Index can be considered as mySQL table name.
+     *
+     * @return string
+     */
+    public function getMeiliSearchIndexName(): string;
+}
