2fed476e7c1081435194fdd0a9d42d6caddf4bf1
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-03-11 15:40:26 +0100

    ✨ Adding MeilisearchContactRelatedResource trait.

--- src/Http/Resources/Account.php
+++ src/Http/Resources/Account.php
@@ -1,4 +1,3 @@
-
 <?php
 namespace Deegitalbe\TrustupProAppCommon\Http\Resources;
 
---
+++ src/Http/Resources/Traits/MeilisearchContactRelatedResource.php
@@ -0,0 +1,29 @@
+<?php
+namespace Deegitalbe\TrustupProAppCommon\Http\Resources\Traits;
+
+use Deegitalbe\TrustupProAppCommon\Http\Resources\Contact;
+
+trait MeilisearchContactRelatedResource
+{
+    /**
+     * Adding contact to resource if contact is loaded.
+     * 
+     * @return mixed
+     */
+    public function addContactWhenLoaded()
+    {
+        $this->when($this->isContactLoaded(), function() {
+                return $this->addContact();
+        });
+    }
+
+    /**
+     * Adding contact to resource.
+     * 
+     * @return mixed
+     */
+    public function addContact()
+    {
+        return new Contact($this->getContact());
+    }
+}
